052d749d84d160947de5e8479d56f4f74aa234d7
EngineerRohit01 <rohitkusv@gmail.com>
2024-08-16 23:12:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Shortener from './Shortener';
+import './App.css';
+
+
+function App(){
+  return (
+    <div className="App">
+      <Shortener />
+    </div>
+  );
+};
+
+export default App;
